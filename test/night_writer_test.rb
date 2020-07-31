fd164dbfadca90b23032e5eced71e1ac504adc31
Austin Aspaas <araspaas@live.com>
2020-07-31 17:18:24 -0600

    Add setup and test it exists

---
+++ test/night_writer_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+
+  def setup
+    @night_writer = NightWriter.new
+  end
+  
+  def test_it_exists
+    assert_instance_of NightWriter, @night_writer
+  end
+end
