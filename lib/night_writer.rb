19a806a3ba9158ba3fada850ef78c6b20e5d3660
Austin Aspaas <araspaas@live.com>
2020-08-01 13:45:03 -0600

    Add the ability to read and write message.txt to braille.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,19 @@
-class NightWriter
+file = File.open(ARGV[0],"r")
 
-end
+incoming_text = file.read
+
+file.close
+
+capitalized_text = incoming_text.upcase
+
+writer = File.open(ARGV[1],"w")
+
+writer.write(capitalized_text)
+
+writer.close
+
+read_new = File.open(ARGV[1],"r")
+
+number_of_characters = read_new.read.length
+
+puts "Created '#{ARGV[1]}' containing #{number_of_characters} characters"
