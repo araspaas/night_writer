e9099bf9ed790ed8ef561fe6355d0a96ce722afa
Austin Aspaas <araspaas@live.com>
2020-08-02 16:03:51 -0600

    Comment out unused method for time being.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -19,15 +19,23 @@ class Encrypt
     @night_writer = NightWriter.new
   end
 
+  # def encrypt_file
+  #   english = @night_writer.read.chomp
+  #   binding.pry
+  #   braille = translate(english)
+  #   @night_writer.write(braille)
+  # end
+
   def translate(letter)
     letters = letter.chars
-    letters.map do |letter|
+    arrays = letters.map do |letter|
       @alphabet[letter]
     end
+    grid_format(arrays)
   end
 
   def grid_format(arrays)
-    x = arrays.map do |array|
+    arrays.map do |array|
       @line1 << array[0]
       @line2 << array[1]
       @line3 << array[2]
@@ -40,6 +48,6 @@ class Encrypt
     line_2_as_string = line2.join('')
     line_3_as_string = line3.join('')
     @all_lines = [line_1_as_string, line_2_as_string, line_3_as_string]
-    all_lines.join("\n")
+    @all_lines.join("\n")
   end
 end
