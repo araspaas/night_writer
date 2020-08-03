cae89d48dc406807e61d2cbd0605f354902a82f0
Austin Aspaas <araspaas@live.com>
2020-08-03 13:58:49 -0600

    Add initialize method and pass test

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,3 +1,11 @@
-class Decrypt
+require_relative 'file_input_output'
+require_relative 'alphabet_hash'
 
+class Decrypt
+  include AlphabetHash
+  attr_reader :alphabet
+
+  def initialize
+    @alphabet = AlphabetHash.alphabet_hash.invert
+  end
 end
